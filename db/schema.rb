a200b8da49ad6d32f559a44f0caee71a104d1763
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-08-31 14:17:40 +0500

    Migrations Updated

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_075953) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "client", force: :cascade do |t|
+  create_table "clients", force: :cascade do |t|
     t.text "name"
     t.text "phone"
     t.text "datestamp"
